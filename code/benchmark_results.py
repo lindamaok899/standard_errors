f2314395613421a0a4e3920ab7fd3531f140cf54
Linda <lindamaok899@gmail.com>
2019-06-06 15:53:47 +0200

    copmares statsmodels covariance matrices outputs to Laura's functions.

---
+++ code/benchmark_results.py
@@ -0,0 +1,101 @@
+import pandas as pd
+import numpy as np
+import statsmodels as sm
+import patsy
+import statsmodels.discrete.discrete_model as smp
+from probit import probit_score_obs, probit_hessian
+from logit import logit_score_obs, logit_hessian
+from standard_errors import cov_hessian, cov_jacobian, cov_sandwich
+
+# ========================================================================================
+# Loading data and neccesary functions to get input data
+# ------------------------------------------------------
+
+spector_data = sm.datasets.spector.load_pandas().data
+formula = "GRADE ~ GPA + TUCE + PSI"
+
+
+def binary_processing(formula, data):
+    """Construct the inputs for the binomial functions.
+    
+    Args:
+        formula (str): A patsy formula
+        data (pd.DataFrame): The dataset
+        
+    Returns:
+        y (np.array): 1d numpy array with the dependent variable
+        x (np.array): 2d numpy array with the independent variables
+        params_sr (pd.Series): The data are naive start values for the parameters.
+            The index contains the parameter names (i.e. names of the variables).
+    
+        
+    Patsy is described here: https://patsy.readthedocs.io/en/latest/
+    
+    Should we rather use DataFrames instead of numpy arrays?
+    
+    """
+    y, x = patsy.dmatrices(formula, data, return_type="dataframe")
+    start_params = np.linalg.lstsq(x, y, rcond=None)[0].ravel()
+    params_sr = pd.Series(data=start_params, index=x.columns, name="value")
+    return y.to_numpy().reshape(len(y)), x.to_numpy(), params_sr
+
+
+# inputs
+y, x, params = binary_processing(formula, spector_data)
+nobs = len(x)
+# =======================================================================================
+# estimate a probit and logit model in statsmodels
+# ------------------------------------------------
+
+y, x, params_sr = binary_processing(formula, spector_data)
+probit_mod = smp.Probit(endog=y, exog=x)
+logit_mod = smp.Logit(endog=y, exog=x)
+
+# fit probit and logit models
+probit_res = probit_mod.fit()
+logit_res = logit_mod.fit()
+print("Parameters: ", probit_res.params)
+print("Parameters: ", logit_res.params)
+# =======================================================================================
+# Estimate hessian and jacobian matrices from statsmodels and Laura's functions
+# -------------------------------------------------------------------------------
+
+# get statsmodels hessian and jacobian matrices
+smprobit_hessian = probit_mod.hessian(params_sr)
+smprobit_jacobian = probit_mod.score_obs(params_sr)
+
+smlogit_hessian = logit_mod.hessian(params_sr)
+smlogit_jacobian = logit_mod.score_obs(params_sr)
+
+# get Lauras hessian and jacobian matrices
+probit_hessian = probit_hessian(params, y, x)
+logit_hessian = logit_hessian(params, y, x)
+
+probit_jacobian = probit_score_obs(params, y, x)
+logit_jacobian = logit_score_obs(params, y, x)
+
+# =======================================================================================
+# benchmark covariance matrices from Lauras's functions against statsmodels
+# --------------------------------------------------------------------------
+
+# compare probit and logit hessian cov matrices
+cov_probit_hessian = cov_hessian(probit_hessian, nobs)
+cov_probit_smhessian = cov_hessian(smprobit_hessian, nobs)
+
+cov_logit_hessian = cov_hessian(logit_hessian, nobs)
+cov_logit_smhessian = cov_hessian(smlogit_hessian, nobs)
+
+# compare probit and logit jacobian cov matrices
+cov_probit_jacobian = cov_jacobian(probit_jacobian, nobs)
+cov_probit_smjacobian = cov_jacobian(smprobit_jacobian, nobs)
+
+cov_logit_jacobian = cov_jacobian(logit_jacobian, nobs)
+cov_logit_smjacobian = cov_jacobian(smlogit_jacobian, nobs)
+
+# comapre probit and logit cov_sandwich results
+covsand_probit_jacobian = cov_sandwich(probit_jacobian, probit_hessian, nobs)
+covsand_probit_jacobiansm = cov_sandwich(smprobit_jacobian, smprobit_hessian, nobs)
+
+covsand_logit_jacobian = cov_sandwich(logit_jacobian, logit_hessian, nobs)
+covsand_logit_jacobiansm = cov_sandwich(smlogit_jacobian, smlogit_hessian, nobs)
+# =======================================================================================
---
+++ code/standard_errors.py
@@ -0,0 +1,112 @@
+import numpy as np
+
+# ======================================================================================
+# Notes
+# -----
+## differences between hessian and jacobian covariance matrices
+
+# in general the hessian and jacobian matrices will not have identical cov matrices.
+# the hessian estimator typically has somewhat better properties in small samples.
+# computation of the jacobian cov matrix requires the individual likelihood
+# contributions
+
+##Scaling
+
+# scaling is not neccesary when using statsmodels and ose discrete models to estimate
+# probit and logit hessian and jacobian covariance matrices as scaling results in very
+# large standard errors.
+# =======================================================================================
+
+
+def cov_hessian(hessian, nobs):
+
+    """Covariance based on the negative inverse of the hessian of loglike.
+    
+    Args:
+        hessian (np.array): 2d array of dimension (nparams, nparams)
+        nobs (scalar): number of observations
+        
+    Returns:
+       hessian_matrix (np.array): 2d array covariance matrix (nparams, nparams)
+       
+   Notes
+       - 
+       - 
+       - computation of the jacobian covariance matrix requires the individual likelihood 
+       contributions.
+     
+   
+    Resources: Marno Verbeek - A guide to modern econometrics.
+    
+    """
+    # info_matrix = -1 * ((hessian)/nobs)
+    info_matrix = -1 * (hessian)
+    cov_hes = np.linalg.inv(info_matrix)
+
+    return cov_hes
+
+
+def cov_jacobian(jacobian, nobs):
+
+    """Covariance based on outer product of jacobian of loglikeobs.
+    
+    Args:
+        jacobian (np.array): 2d array of dimension (nobs, nparams)
+        nobs (scalar): number of observations
+        
+    Returns:
+        jacobian_matrix (np.array): 2d array covariance matrix (nparams, nparams)
+        
+     Notes:
+       - in general the hessian and jacobian matrices will not have identical covariance
+       matrices.
+       - the hessian estimator typically has somewhat better properties in small samples.
+       -computation of the jacobian covariance matrix requires requires the individual 
+       likelihood contributions
+        
+    Resources: Marno Verbeek - A guide to modern econometrics.
+    
+    """
+
+    # info_matrix = np.dot((jacobian.T), jacobian)/nobs
+    info_matrix = np.dot((jacobian.T), jacobian)
+    cov_jac = np.linalg.inv(info_matrix)
+
+    return cov_jac
+
+
+def cov_sandwich(jacobian, hessian, nobs):
+
+    """Covariance of parameters based on HJJH dot product of Hessian, Jacobian, Jacobian, Hessian of likelihood.
+    
+    Args:
+        jacobian (np.array): 2d array of dimension (nobs, nparams)
+        nobs (scalar): number of observations
+        
+    Returns:
+        sandwich_cov (np.array): 2d array covariance HJJH matrix (nparams, nparams)
+        
+    Resources:
+    (https://github.com/statsmodels/statsmodels/blob/a33424eed4cacbeacc737d40fe5700daf39463f6/statsmodels/base/model.py#L2194)
+    
+    """
+
+    # info_matrix = np.dot((jacobian.T), jacobian)/nobs
+    info_matrix = np.dot((jacobian.T), jacobian)
+    cov_hes = cov_hessian(hessian, nobs)
+    sandwich_cov = np.dot(cov_hes, np.dot(info_matrix, cov_hes))
+
+    return sandwich_cov
+
+
+def se_calculations(function):
+
+    """standard deviation of parameter estimates based on the function of choice.
+    
+    Returns:
+        standard_errors (np.array): 1d array of dimension (nparams) with standard errors.
+    
+    """
+    standard_errors = np.sqrt(np.diag(function()))
+
+    return standard_errors
